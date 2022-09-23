edddbd838a9ca5186ad902ef178f07c72c2d1291
Mateusz-Trojnar <mateusztrojnar60@gmail.com>
2022-09-23 22:13:32 +0200

    kalkulator-kosztow-wynajmu-js

--- scripts/script.js
+++ scripts/script.js
@@ -65,82 +65,89 @@ function getCarRentCost() {
         kilometers: kilometers,
         fuelCost: fuelCost,
         result: result,
+        receptionCar: receptionCar,
+        returnCar: returnCar,
     }
     return data
 }
 
 document.querySelector("#submit").addEventListener("click", () => {
 
+
     const data = getCarRentCost()
 
-
-    if (data.newDrivingLicense < 5) {
-
-        if (data.newDrivingLicense < 3 && data.categoryField == "premium") {
-            document.querySelector("#result").innerHTML = `Nie możesz wypożyczyć samochodu wersji premium`
-        } else {
-
-            switch (data.categoryField) {
-                case 'basic':
-                    data.price *= 1
-                    break
-                case 'standard':
-                    data.price *= 1.3
-                    break
-                case 'medium':
-                    data.price *= 1.6
-                    break
-                case 'premium':
-                    data.price *= 2
-                    break
-                default:
-                    data.price *= 1
-            }
-
-            if (data.carModelQuantity > 3) {
-                const nettoPrice = (data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromDrivingLicense
-                const bruttoPrice = nettoPrice * vatValue
-                document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
-            } else if (data.carModelQuantity <= 3) {
-                const nettoPrice = ((data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromDrivingLicense) * percentageFromQuantity
-                const bruttoPrice = nettoPrice * vatValue
-                document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
-            }
-
-        }
+    if (data.kilometers.length == 0 || data.yearOfReceiptOfDrivingLicense.length == 0 || data.receptionCar.length == 0 || data.returnCar.length == 0) {
+        document.querySelector("#result").innerHTML = `Wypełnij wszystkie pola`
     } else {
 
-        if (data.newDrivingLicense < 3 && data.categoryField == "premium") {
-            document.querySelector("#result").innerHTML = `Nie możesz wypożyczyć samochodu wersji premium`
+        if (data.newDrivingLicense < 5) {
+
+            if (data.newDrivingLicense < 3 && data.categoryField == "premium") {
+                document.querySelector("#result").innerHTML = `Nie możesz wypożyczyć samochodu wersji premium`
+            } else {
+
+                switch (data.categoryField) {
+                    case 'basic':
+                        data.price *= 1
+                        break
+                    case 'standard':
+                        data.price *= 1.3
+                        break
+                    case 'medium':
+                        data.price *= 1.6
+                        break
+                    case 'premium':
+                        data.price *= 2
+                        break
+                    default:
+                        data.price *= 1
+                }
+
+                if (data.carModelQuantity > 3) {
+                    const nettoPrice = (data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromDrivingLicense
+                    const bruttoPrice = nettoPrice * vatValue
+                    document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
+                } else if (data.carModelQuantity <= 3) {
+                    const nettoPrice = ((data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromDrivingLicense) * percentageFromQuantity
+                    const bruttoPrice = nettoPrice * vatValue
+                    document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
+                }
+            }
+
         } else {
 
-            switch (data.categoryField) {
-                case 'basic':
-                    data.price *= 1
-                    break
-                case 'standard':
-                    data.price *= 1.3
-                    break
-                case 'medium':
-                    data.price *= 1.6
-                    break
-                case 'premium':
-                    data.price *= 2
-                    break
-                default:
-                    data.price *= 1
-            }
+            if (data.newDrivingLicense < 3 && data.categoryField == "premium") {
+                document.querySelector("#result").innerHTML = `Nie możesz wypożyczyć samochodu wersji premium`
+            } else {
 
-            if (data.carModelQuantity > 3) {
-                const nettoPrice = data.result * priceForOneNight + data.price + data.fuelCost
-                const bruttoPrice = nettoPrice * vatValue
-                document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
-            } else if (data.carModelQuantity <= 3) {
-                const nettoPrice = (data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromQuantity
-                const bruttoPrice = nettoPrice * vatValue
-                document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
-            }
+                switch (data.categoryField) {
+                    case 'basic':
+                        data.price *= 1
+                        break
+                    case 'standard':
+                        data.price *= 1.3
+                        break
+                    case 'medium':
+                        data.price *= 1.6
+                        break
+                    case 'premium':
+                        data.price *= 2
+                        break
+                    default:
+                        data.price *= 1
+                }
 
+                if (data.carModelQuantity > 3) {
+                    const nettoPrice = data.result * priceForOneNight + data.price + data.fuelCost
+                    const bruttoPrice = nettoPrice * vatValue
+                    document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
+                } else if (data.carModelQuantity <= 3) {
+                    const nettoPrice = (data.result * priceForOneNight + data.price + data.fuelCost) * percentageFromQuantity
+                    const bruttoPrice = nettoPrice * vatValue
+                    document.querySelector("#result").innerHTML = `Cena netto: ${nettoPrice.toFixed(2)} zł <br>Cena brutto: ${bruttoPrice.toFixed(2)} zł<br> W tym: koszty paliwa: ${data.fuelCost.toFixed(2)}zł, koszt wypożyczenia na ${data.result} dni: ${(nettoPrice - data.fuelCost).toFixed(2)}zł`
+                }
+
+            }
         }
     }
 })
